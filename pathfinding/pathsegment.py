112f62a60d15206c95b0e0cb3c19e4fb8cf29ffa
MoiShroomy <mail2rlee82@yahoo.com.sg>
2025-08-26 02:04:01 +0800

    Preperation for PathSegment class

---
+++ pathfinding/pathsegment.py
@@ -0,0 +1,7 @@
+
+class PathSegment:
+
+    """
+    This class represents a single segment of a path (either a curve or a straight)
+    """
+    #TODO Actually implement the path segment class lol
