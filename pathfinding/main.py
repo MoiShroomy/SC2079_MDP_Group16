50726ab18f1dacc8ec4aa6fbb85ec514c24745e8
raye <mail2rlee82@yahoo.com.sg>
2025-09-05 09:51:41 +0800

    Added Reeds Shepp

--- pathfinding/main.py
+++ pathfinding/main.py
@@ -1,6 +1,8 @@
 from world_execute_me.map import Map
 from 縁結び.dubins import Dubins
+from 縁結び.reeds_shepp import RS
 
 d = Dubins(20)
+r = RS()
 m = Map([])
-print(m.generate_edges([(0,0,0), (40, 70, 0)], d))
+print(m.generate_edges([(0,0,0), (40, 70, 0)], r))
--- pathfinding/utils.py
+++ pathfinding/utils.py
@@ -1,4 +1,4 @@
-from math import pi
+from math import pi, sqrt, atan2
 
 def rad2deg(rad):
     """
@@ -11,3 +11,20 @@ def deg2rad(deg):
     Converts degrees to radians
     """
     return pi * deg / 180
+
+def M(theta):
+        """
+        Return the angle phi = theta mod (2 pi) such that -pi <= theta < pi.
+        """
+        theta = theta % (2*pi)
+        if theta < -pi: return theta + 2*pi
+        if theta >= pi: return theta - 2*pi
+        return theta
+
+def R(x, y):
+    """
+    Return the polar coordinates (r, theta) of the point (x, y).
+    """
+    r = sqrt(x*x + y*y)
+    theta = atan2(y, x)
+    return r, theta
